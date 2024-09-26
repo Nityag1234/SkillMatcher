232010a155164ba3c835b54fe37c0195f41237a1
Nityag1234 <nitya12343@gmail.com>
2024-09-26 09:15:16 +0530

    modified frontend

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,21 +1,21 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 
 
 //import cors from 'cors';
 const app = express()
-// Set up CORS
+//Set up CORS
 app.use(cors({
     origin: 'http://localhost:3000', // Frontend URL
     credentials: true // Allow cookies to be sent
 }));
+//app.use(cors(corsOptions));
 
 
-
-app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+// app.use(cors({
+//     origin: process.env.CORS_ORIGIN,
+//     credentials: true
+// }))
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
